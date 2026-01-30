cd0f78968dd28301bc5c4514348a83c30f38a2ed
FlyingTiger12345 <matthewvasquez1434@gmail.com>
2026-01-30 19:56:40 +0800

    123

---
+++ class6/and or.py
@@ -0,0 +1,15 @@
+a = 10
+b = 12
+c = 0
+if a and b and c:
+    print("all the boolean value true")
+else:
+    print("only one  boolean value ")
+
+f = 10
+e = 12
+d = 0
+if f > 0 or e > 0:
+    print(" 1  variable is Greater than zero")
+else:
+    print("No variable greater than zero")
