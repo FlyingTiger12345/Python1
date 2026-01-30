c1595afb65c4d9a6449f6ac6687cd6cd0563ac92
FlyingTiger12345 <matthewvasquez1434@gmail.com>
2026-01-30 20:09:11 +0800

    123

---
+++ class6/assigment.py
@@ -0,0 +1,13 @@
+w = input("enter alphabet")
+if len(w)>1:
+    print("not alphabet")
+
+else:
+    if w>="a" and w>="z":
+        print("alphabet good")
+    elif w>="A" and w>="Z":
+        print( "ALPHABET GOOD")
+    else:
+        print( "alphabet not goodd")
+
+    
