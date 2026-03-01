fa18c49a4640ed79531e35e09fd8e85c9589a7b0
FlyingTiger12345 <matthewvasquez1434@gmail.com>
2026-03-01 20:28:39 +0800

    1213123

---
+++ CLASS14/assignment.py
@@ -0,0 +1,11 @@
+shop = input("do u want tto buy 67 dollar item y/n")
+wallet = 500
+
+def shop12():
+    if shop == "y":
+        print(wallet-67)
+    elif shop == "n":
+        print("ok bye")
+    else:
+        print("ok")
+shop12()
