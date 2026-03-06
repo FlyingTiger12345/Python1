90254426503b59641c8b0b0f1b2cc66f560f73e6
FlyingTiger12345 <matthewvasquez1434@gmail.com>
2026-03-06 19:58:02 +0800

    213`413425

---
+++ class15/AFTERCLASSS.py
@@ -0,0 +1,10 @@
+
+try:
+    a =int(input("enter age"))
+    if a%2 == 0:
+        print("this is even number")
+    else:
+        print("this odd number")
+except ValueError as amongus:
+    print("numbers only")
+
