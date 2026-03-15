e310bc554dc233430e9af0007ef40912c3c59752
FlyingTiger12345 <matthewvasquez1434@gmail.com>
2026-03-15 20:27:49 +0800

    234523521345

---
+++ class18/lists.py
@@ -0,0 +1,19 @@
+print("empty list")
+list1 = []
+print(list1)
+
+print("numbers list")
+list2 = [67,21,45,62,7652,54]
+print(list2)
+
+
+
+print("multiplied by 6  list"      )
+list3 = [670,677,6767]
+sublist3 = list3*6
+print(list3,sublist3)
+
+
+print("reverselist")
+list2.reverse()
+print(list2)
