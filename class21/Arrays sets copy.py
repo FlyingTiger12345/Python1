4078932c9d56ad3806c30b29f3fbe56cad68bee1
FlyingTiger12345 <matthewvasquez1434@gmail.com>
2026-03-27 20:01:13 +0800

    234523423423

---
+++ class21/Arrays sets copy.py
@@ -0,0 +1,9 @@
+set1 = {"red","blue"}
+set2 = {"greens","red"}
+
+
+print(set1)
+print(set2)
+print(set1.union(set2))
+print(set1.intersection(set2))
+print(set1.difference(set2))
---
+++ class21/Arrays sets.py
@@ -0,0 +1,19 @@
+numberlisst = {1,2,3}
+print(numberlisst)
+
+numberlisst = {1,2,3,(1,2,3)}
+print(numberlisst)
+
+numberlisst = {1,2,3,"123"}
+print(numberlisst)
+
+numberlisst = {1,2,3,3,4,5}
+print(numberlisst)
+
+
+numberlisst = set ([1,2,3])
+print(numberlisst)
+
+
+numberlisst.pop()
+print(numberlisst)
