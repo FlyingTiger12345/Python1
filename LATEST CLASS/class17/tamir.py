540a556d82245f9fa12ca628cc185441b7b19470
FlyingTiger12345 <matthewvasquez1434@gmail.com>
2026-09-20 19:56:37 +0800

    312312312

---
+++ LATEST CLASS/class17/tamir.py
@@ -0,0 +1,36 @@
+import requests
+
+def get_random_joke():
+    url = "https://official-joke-api.appspot.com/random_joke"
+    response = requests.get(url)
+
+
+    if response.status_code == 200:
+        print(f"Full JSON Response: {response.json()}")
+
+
+
+        joke_data=response.json()
+        return f"{joke_data['setup']}- {joke_data['punchline']}"
+    else:
+        return "Failed to retrieve joke"
+
+
+def main():
+    print("welcome to random jjoke generator")
+
+    while True:
+        user_input = input("press enter to get new joke or type 'q'/'exit' to exit").strip().lower()
+
+        if user_input in ("q", "exit"):
+            print("goodbye")
+            break
+
+
+        joke = get_random_joke()
+        print(joke)
+
+
+
+if __name__ == "__main__":
+    main()
